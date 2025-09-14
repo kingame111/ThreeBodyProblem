4509b38234d1f7aba00cb6a5d0fb93052fb29af4
kingame111 <your_email@example.com>
2025-09-14 12:14:32 +0300

    finished creation

--- src/main/java/org/example/Equations.java
+++ src/main/java/org/example/Equations.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Equations {
 Variables body [] = new Variables[3];
+final double G = 6.67430*Math.pow(10, -11);
 
+public double distance (Variables a, Variables b){
+    double total =  Math.pow(a.getX()- b.getX(),2) + Math.pow(a.getY()- b.getY(),2) Math.pow(a.getZ()- b.getZ(),2);
+   total = Math.sqrt(total);
+
+}
+public double gravity(Variables x, Variables y){
+        double size =
+
+
+    }
 
 }
