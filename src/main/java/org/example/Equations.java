d66d5131941733ce42afded780f0e9b28cb61ce5
kingame111 <your_email@example.com>
2025-09-14 15:01:47 +0300

    finished creation

--- src/main/java/org/example/Equations.java
+++ src/main/java/org/example/Equations.java
@@ -4,13 +4,16 @@ public class Equations {
 Variables body [] = new Variables[3];
 final double G = 6.67430*Math.pow(10, -11);
 
-public double distance (Variables a, Variables b){
-    double total =  Math.pow(a.getX()- b.getX(),2) + Math.pow(a.getY()- b.getY(),2) Math.pow(a.getZ()- b.getZ(),2);
-   total = Math.sqrt(total);
+public double distancesqr (Variables a, Variables b){
+    for (int i = 0; i< 3; i++){
+    double total =  Math.pow(a.getR()[i]-b.getR()[i],2);
+    total = total + total;
+    }
+   return total;
 
 }
-public double gravity(Variables x, Variables y){
-        double size =
+public double gravity(Variables a, Variables b){
+        double size = (G*a.getM()*b.getM())/distancesqr(a,b);
 
 
     }
--- src/main/java/org/example/Variables.java
+++ src/main/java/org/example/Variables.java
@@ -1,30 +1,33 @@
 package org.example;
 
 public class Variables {
-    private double x,y,z;
-    private double Vx,Vy,Vz;
+    private double[] r = new double[3];
+    private double[] v = new double[3];
     private double m;
 
 
     // ---getters---
 
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public double getZ() { return z; }
-    public double getVx() { return Vx; }
-    public double getVy() { return Vy; }
-    public double getVz() { return Vz; }
+    public double[] getR() { return r; }
+
+    public double[] getV() { return v; }
+
     public double getM() { return m; }
 
 
 // ---setters---
 
-    public void setX (double x){ this.x = x;}
-    public void setY (double y){ this.y = y;}
-    public void setZ (double z){ this.z = z;}
-    public void setVx (double Vx){ this.Vx = Vx;}
-    public void setVy (double Vy){ this.Vy = Vy;}
-    public void setVz (double Vz){ this.Vz = Vz;}
+    public void setR (double x, double y, double z){
+        this.r[0] = x;
+        this.r[0] = y;
+        this.r[0] = z;
+    }
+
+    public void setV (double vx, double vy, double vz){
+        this.v[0] = vx;
+        this.v[0] = vy;
+        this.v[0] = vz;
+    }
     public void setM (double m){
         if ( m > 0){
             this.m = m;
